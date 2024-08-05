f1f7a953cb8632a11bd5debac4ba8ebc82da7289
priyesh-xX <129549189+priyesh-xX@users.noreply.github.com>
2024-08-05 12:29:23 +0530

    Update HowItWorks.jsx

--- JobSeeking/frontend/src/components/Home/HowItWorks.jsx
+++ JobSeeking/frontend/src/components/Home/HowItWorks.jsx
@@ -6,37 +6,34 @@ import { IoMdSend } from "react-icons/io";
 const HowItWorks = () => {
   return (
     <>
-      <div className="howitworks">
-        <div className="container">
-          <h3>How JobZee Works</h3>
-          <div className="banner">
-            <div className="card">
-              <FaUserPlus />
-              <p>Create Account</p>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
-              </p>
-            </div>
-            <div className="card">
-              <MdFindInPage />
-              <p>Find a Job/Post a Job</p>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
-              </p>
-            </div>
-            <div className="card">
-              <IoMdSend />
-              <p>Apply For Job/Recruit Suitable Candidates</p>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
-              </p>
-            </div>
-          </div>
-        </div>
+     <div className="howitworks">
+  <div className="container">
+    <h3>How JobZee Works</h3>
+    <div className="banner">
+      <div className="card">
+        <FaUserPlus />
+        <p>Create Account</p>
+        <p>
+          Sign up to access our job portal and start your journey towards a better career or find the right candidates for your company.
+        </p>
       </div>
+      <div className="card">
+        <MdFindInPage />
+        <p>Find a Job/Post a Job</p>
+        <p>
+          Explore thousands of job listings to find your ideal position or post job openings to attract qualified candidates.
+        </p>
+      </div>
+      <div className="card">
+        <IoMdSend />
+        <p>Apply For Job/Recruit Suitable Candidates</p>
+        <p>
+          Submit your application to your desired job or review applications and recruit the best candidates for your company.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
     </>
   );
 };
